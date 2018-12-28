71bb24dc627c61a695bb5e5c70e5998791e1a866
gurv-s <vargwin@gmail.com>
2018-12-28 20:45:26 +0530

    Add soundex US, move jarowinkler to separate module

---
+++ src/jarowinkler.rs
@@ -0,0 +1,167 @@
+use std::cmp;
+
+/// A simple mutable implementation of Jaro-Winkler to
+/// keep memory allocations minimum.
+pub struct JaroWinkler {
+    empty: Vec<bool>,
+    min_flags: Vec<bool>,
+    max_flags: Vec<bool>,
+}
+
+impl JaroWinkler {
+    pub fn new() -> JaroWinkler {
+        JaroWinkler {
+            empty: vec![false; 128],
+            min_flags: vec![false; 128],
+            max_flags: vec![false; 128],
+        }
+    }
+
+    /// Match two input strings and produces a score between 0 and 1.
+    pub fn apply(&mut self, s1: &str, s2: &str) -> f64 {
+        if s1.is_empty() && s2.is_empty() {
+            return 1.0;
+        }
+
+        if s1.is_empty() || s2.is_empty() {
+            return 0.0;
+        }
+
+        if s1 == s2 {
+            return 1.0;
+        }
+
+        let b1 = s1.as_bytes();
+        let b2 = s2.as_bytes();
+
+        let max_len = cmp::max(b1.len(), b2.len());
+        self.ensure_capacity(max_len);
+
+        if b1.len() > b2.len() {
+            self.calculate(b2, b1)
+        } else {
+            self.calculate(b1, b2)
+        }
+    }
+
+    fn ensure_capacity(&mut self, capacity: usize) {
+        let current_capacity = self.empty.len();
+        if capacity <= current_capacity {
+            return;
+        }
+
+        let mut new_capacity = current_capacity << 1;
+        if new_capacity < capacity {
+            new_capacity = capacity;
+        }
+        self.empty.resize(new_capacity, false);
+        self.min_flags.resize(new_capacity, false);
+        self.max_flags.resize(new_capacity, false);
+    }
+
+    fn calculate(&mut self, min: &[u8], max: &[u8]) -> f64 {
+        let m = self.matches(min, max);
+
+        if m == 0.0 {
+            return 0.0;
+        }
+
+        let t = self.transpositions(min, max);
+        let p = self.prefix(min, max);
+        let min_len = min.len() as f64;
+        let max_len = max.len() as f64;
+
+        let j = (m / min_len + m / max_len + (m - t) / m) / 3.0;
+        j + 0.1 * p * (1.0 - j)
+    }
+
+    fn matches(&mut self, min: &[u8], max: &[u8]) -> f64 {
+        let range = cmp::max(max.len() / 2 - 1, 0);
+        let mut matches = 0.0;
+
+        let min_flags = &mut self.min_flags;
+        let max_flags = &mut self.max_flags;
+
+        for i in 0..min.len() {
+            let c = min[i];
+
+            let start = if i > range { i - range } else { 0 };
+            let end = cmp::min(i + range + 1, max.len());
+
+            for j in start..end {
+                if !max_flags[j] && c == max[j] {
+                    min_flags[i] = true;
+                    max_flags[j] = true;
+                    matches += 1.0;
+                    break;
+                }
+            }
+        }
+        matches
+    }
+
+    fn transpositions(&mut self, min: &[u8], max: &[u8]) -> f64 {
+        let mut t = 0;
+        let mut j = 0;
+
+        let min_flags = &mut self.min_flags;
+        let max_flags = &mut self.max_flags;
+
+        for i in 0..min.len() {
+            if !min_flags[i] {
+                continue;
+            }
+            while j < max.len() && !max_flags[j] {
+                j += 1;
+            }
+            if min[i] != max[j] {
+                t += 1;
+            }
+            j += 1;
+        }
+
+        let empty = &self.empty[0..max.len()];
+        min_flags[0..max.len()].copy_from_slice(empty);
+        max_flags[0..max.len()].copy_from_slice(empty);
+
+        (t / 2) as f64
+    }
+
+    fn prefix(&self, min: &[u8], max: &[u8]) -> f64 {
+        let mut prefix = 0.0;
+        for i in 0..cmp::min(min.len(), 4) {
+            if min[i] == max[i] {
+                prefix += 1.0;
+            } else {
+                break;
+            }
+        }
+        prefix
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::JaroWinkler;
+
+    #[test]
+    fn partial_match() {
+        let mut jw = JaroWinkler::new();
+        let score = jw.apply("Foo bar", "Food candybar");
+        assert_eq!(score, 0.7897435897435898);
+    }
+
+    #[test]
+    fn full_match() {
+        let mut jw = JaroWinkler::new();
+        let score = jw.apply("Foo bar", "Foo bar");
+        assert_eq!(score, 1.0);
+    }
+
+    #[test]
+    fn no_match() {
+        let mut jw = JaroWinkler::new();
+        let score = jw.apply("Foobar", "pqxyz");
+        assert_eq!(score, 0.0);
+    }
+}
---
+++ src/soundex.rs
@@ -0,0 +1,55 @@
+const SOUNDEX_MAP: &[u8] = b"01230120022455012623010202";
+
+pub fn soundex(s: &str) -> String {
+    if s.is_empty() {
+        return "".to_string();
+    }
+
+    let s = clean(s);
+    let b = s.as_bytes();
+    let first = b[0];
+    let mut result = vec![first, b'0', b'0', b'0'];
+    let mut last = map(first);
+    let mut count = 1;
+
+    for i in 1..b.len() {
+        if count > 3 {
+            break;
+        }
+
+        let c = match b[i] {
+            b'H' | b'W' => continue,
+            c => map(c),
+        };
+
+        if c == b'-' {
+            continue;
+        } else if c != b'0' && c != last {
+            result[count] = c;
+            count += 1;
+        }
+
+        last = c;
+    }
+
+    String::from_utf8(result).unwrap()
+}
+
+fn map(c: u8) -> u8 {
+    let i = c - b'A';
+    SOUNDEX_MAP[i as usize]
+}
+
+fn clean(s: &str) -> String {
+    if s.is_empty() {
+        return "".to_string();
+    }
+
+    let mut out = String::with_capacity(s.len());
+    for c in s.chars() {
+        if c.is_alphabetic() {
+            out.push(c);
+        }
+    }
+    out.to_uppercase()
+}
