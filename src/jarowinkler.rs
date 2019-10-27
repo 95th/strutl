84720c210d19cde35d2a41445fdcb5ab378a2444
Gurwinder Singh <vargwin@gmail.com>
2019-10-28 00:27:24 +0530

    Improve cache locality by using single vec

--- src/jarowinkler.rs
+++ src/jarowinkler.rs
@@ -1,11 +1,8 @@
-use std::cmp;
-
 /// A simple mutable implementation of Jaro-Winkler to
 /// keep memory allocations minimum.
 #[derive(Default)]
 pub struct JaroWinkler {
-    min_indices: Vec<isize>,
-    max_flags: Vec<bool>,
+    indices: Vec<isize>,
 }
 
 impl JaroWinkler {
@@ -14,10 +11,8 @@ impl JaroWinkler {
     }
 
     pub fn with_size(size: usize) -> Self {
-        assert_ne!(size, 0);
         JaroWinkler {
-            min_indices: vec![-1; size],
-            max_flags: vec![false; size],
+            indices: vec![-1; size],
         }
     }
 
@@ -31,21 +26,19 @@ impl JaroWinkler {
             return 0.0;
         }
 
-        let b1 = s1.as_bytes();
-        let b2 = s2.as_bytes();
+        let mut b1 = s1.as_bytes();
+        let mut b2 = s2.as_bytes();
 
-        let max_len = cmp::max(b1.len(), b2.len());
-        self.ensure_capacity(max_len);
+        self.ensure_capacity(b1.len() + b2.len());
 
         if b1.len() > b2.len() {
-            self.calculate(b2, b1)
-        } else {
-            self.calculate(b1, b2)
+            std::mem::swap(&mut b1, &mut b2);
         }
+        self.calculate(b1, b2)
     }
 
     fn ensure_capacity(&mut self, capacity: usize) {
-        let current_capacity = self.min_indices.len();
+        let current_capacity = self.indices.len();
         if capacity <= current_capacity {
             return;
         }
@@ -54,108 +47,100 @@ impl JaroWinkler {
         if new_capacity < capacity {
             new_capacity = capacity;
         }
-        self.min_indices = vec![-1; new_capacity];
-        self.max_flags = vec![false; new_capacity];
+        self.indices = vec![-1; new_capacity];
     }
 
     fn calculate(&mut self, min: &[u8], max: &[u8]) -> f64 {
-        let m = self.matches(min, max);
+        let mut inner = Inner::new(&mut self.indices, min, max);
+        let m = inner.matches();
 
-        if m == 0.0 {
+        if m == 0 {
             return 0.0;
         }
 
-        let t = self.transpositions(min, max);
-        let p = self.prefix(min, max);
+        let t = inner.transpositions(m);
+        let p = inner.prefix();
         let min_len = min.len() as f64;
         let max_len = max.len() as f64;
+        let m = f64::from(m);
 
         let j = (m / min_len + m / max_len + (m - t) / m) / 3.0;
         j + 0.1 * p * (1.0 - j)
     }
+}
 
-    fn matches(&mut self, min: &[u8], max: &[u8]) -> f64 {
-        let range = cmp::max(max.len() / 2 - 1, 0);
+struct Inner<'a> {
+    min_indices: &'a mut [isize],
+    max_flags: &'a mut [isize],
+    min: &'a [u8],
+    max: &'a [u8],
+}
+
+impl<'a> Inner<'a> {
+    fn new(indices: &'a mut [isize], min: &'a [u8], max: &'a [u8]) -> Self {
+        let (min_indices, max_flags) = indices.split_at_mut(min.len());
+        let max_flags = &mut max_flags[..max.len()];
+        Self {
+            min_indices,
+            max_flags,
+            min,
+            max,
+        }
+    }
+
+    fn matches(&mut self) -> u32 {
+        let range = (self.max.len() / 2 - 1).max(0);
         let mut matches = 0;
         let mut index = 0;
-        for (i, c1) in min.iter().enumerate() {
+        for (i, c1) in self.min.iter().enumerate() {
             let start = if i > range { i - range } else { 0 };
-            let end = cmp::min(i + range + 1, max.len());
+            let end = (i + range + 1).min(self.max.len());
 
-            for (j, c2) in max.iter().enumerate().take(end).skip(start) {
-                if !self.max_flags[j] && c1 == c2 {
+            for (j, c2) in self.max.iter().enumerate().take(end).skip(start) {
+                if self.max_flags[j] != 0 && c1 == c2 {
                     self.min_indices[index] = i as isize;
-                    self.max_flags[j] = true;
+                    self.max_flags[j] = 0;
                     index += 1;
                     matches += 1;
                     break;
                 }
             }
         }
-        f64::from(matches)
+        matches
     }
 
-    fn transpositions(&mut self, min: &[u8], max: &[u8]) -> f64 {
+    fn transpositions(&mut self, matches: u32) -> f64 {
         let mut t = 0;
         let mut max_index = 0;
 
-        for i in 0..min.len() {
-            let min_index = match self.min_indices[i] {
-                v if v < 0 => break,
-                v => v as usize,
-            };
+        for i in 0..matches as usize {
+            let min_index = self.min_indices[i] as usize;
 
-            while !self.max_flags[max_index] {
+            while self.max_flags[max_index] != 0 {
                 max_index += 1;
             }
 
-            if min[min_index] != max[max_index] {
+            if self.min[min_index] != self.max[max_index] {
                 t += 1;
             }
 
             self.min_indices[i] = -1;
-            self.max_flags[max_index] = false;
+            self.max_flags[max_index] = -1;
             max_index += 1;
         }
 
         f64::from(t / 2)
     }
 
-    fn prefix(&self, min: &[u8], max: &[u8]) -> f64 {
+    fn prefix(&self) -> f64 {
         let mut prefix = 0;
-        for i in 0..cmp::min(min.len(), 4) {
-            if min[i] == max[i] {
+        for i in 0..self.min.len().min(4) {
+            if self.min[i] == self.max[i] {
                 prefix += 1;
             } else {
                 break;
             }
         }
         f64::from(prefix)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::JaroWinkler;
-
-    #[test]
-    fn partial_match() {
-        let mut jw = JaroWinkler::new();
-        let score = jw.apply("Foo bar", "Food candybar");
-        assert_eq!(score, 0.7897435897435898);
-    }
-
-    #[test]
-    fn full_match() {
-        let mut jw = JaroWinkler::new();
-        let score = jw.apply("Foo bar", "Foo bar");
-        assert_eq!(score, 1.0);
-    }
-
-    #[test]
-    fn no_match() {
-        let mut jw = JaroWinkler::new();
-        let score = jw.apply("Foobar", "pqxyz");
-        assert_eq!(score, 0.0);
     }
 }
